bf4f516470fe3e7654e5ff5578130580b4d2d56c
Simon Truong <78322463+simont-17@users.noreply.github.com>
2026-01-19 15:13:45 -0700

    added buttons in hero section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,7 @@
-import hero_background from '../images/hero_background.jpg'
+
 import NavBar from "../components/NavBar";
+import { Link } from "react-router-dom";
+
 import '../style/Home.css'
 export default function HomePage() {
     return (
@@ -9,6 +11,10 @@ export default function HomePage() {
             <div className='hero-text-container'>
                 <h1>BUILD YOUR DREAM KITCHEN</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit laudantium iste in harum veritatis ipsam minima saepe, ducimus assumenda tempora necessitatibus voluptates minus laboriosam id magnam labore inventore. Ullam, minus.</p>
+                <div className="hero-button-container">
+                    <Link to="/portfolio" className="hero-work-button">View Our Work</Link>
+                    <Link to="/contact" className="hero-contact-button">Contact Us</Link>
+                </div>
             </div>
 
         </div>
