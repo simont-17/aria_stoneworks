dfbcc6e35f83472f21c15a5cd5378152f046e4c5
Simon Truong <78322463+simont-17@users.noreply.github.com>
2026-01-19 14:50:45 -0700

    styling and text body for home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,15 @@
 import hero_background from '../images/hero_background.jpg'
 import NavBar from "../components/NavBar";
-import '../style/About.css'
+import '../style/Home.css'
 export default function HomePage() {
     return (
         <>
         <div className='hero-container'>
-        <NavBar/>
+            <NavBar/>
+            <div className='hero-text-container'>
+                <h1>BUILD YOUR DREAM KITCHEN</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit laudantium iste in harum veritatis ipsam minima saepe, ducimus assumenda tempora necessitatibus voluptates minus laboriosam id magnam labore inventore. Ullam, minus.</p>
+            </div>
 
         </div>
         </>
