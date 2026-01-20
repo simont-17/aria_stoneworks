a4ec86c25d277a834a2773e53112cabde730cbe9
Zaarif-Sardar <zsard040@mtroyal.ca>
2026-01-20 14:22:13 -0700

    contact page started
    only added hero image and begun work on building contact page. Comp started buggin out. could not contionue and now have to put fries in the bag.

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,9 +1,18 @@
 import NavBar from "../components/NavBar";
+import '../style/Contact.css'
 
 export default function ContactPage() {
     return (
         <>
-         <NavBar/>
+            <div className="contact-hero-container">
+                <NavBar />
+                <div className="hero-text-container">
+                    <h1>
+                        Contact us
+                    </h1>
+
+                </div>
+            </div>
         </>
     )
 }
