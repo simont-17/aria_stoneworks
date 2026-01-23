742a9cee4da7185d430e5ea16bd9aaeb69b3eec4
Simon Truong <78322463+simont-17@users.noreply.github.com>
2026-01-23 11:39:56 -0700

    some styling and testing for portfolio

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -1,4 +1,6 @@
 import NavBar from "../components/NavBar";
+import test1 from "../images/hero_background.jpg"
+import test2 from "../images/pexels-houzlook-3926542.jpg"
 
 import '../style/Portfolio.css'
 
@@ -12,7 +14,28 @@ export default function PortfolioPage() {
                 </div>
             </div>
             <div className='gallery-container'>
+                <div className='gallery-row-container'>
+                    <div className='gallery-column gallery-column-1'>
+                        <img src={test1}></img>
+                        <img src={test1}></img>
+                        <img src={test2}></img>
+                    </div>
+                    <div className='gallery-column gallery-column-2'>
+                        <img src={test1}></img>
+                        <img src={test2}></img>
+                        <img src={test2}></img>
 
+
+                    </div>
+                    <div className='gallery-column gallery-column-3'>
+                        <img src={test1}></img>
+                        <img src={test2}></img>
+                        <img src={test2}></img>
+
+
+                    </div>
+
+                </div>
 
             </div>
         </>
