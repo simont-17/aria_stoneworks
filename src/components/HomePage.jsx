efd0bd9475c3cf99c647284601a3a4947db387b9
Simon Truong <78322463+simont-17@users.noreply.github.com>
2026-01-26 16:34:49 -0700

    styling to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,24 +8,26 @@ import '../style/Home.css'
 export default function HomePage() {
     return (
         <>
-        <div className='hero-container'>
-            <NavBar/>
-            <div className='hero-text-container'>
-                <h1>BUILD YOUR DREAM KITCHEN</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit laudantium iste in harum veritatis ipsam minima saepe, ducimus assumenda tempora necessitatibus voluptates minus laboriosam id magnam labore inventore. Ullam, minus.</p>
-                <div className="hero-button-container">
-                    <Link to="/portfolio" className="hero-work-button">View Our Work</Link>
-                    <Link to="/contact" className="hero-contact-button">Contact Us</Link>
+        <div className='home-container'>
+            <div className='hero-container'>
+                <NavBar/>
+                <div className='hero-text-container'>
+                    <h1>BUILD YOUR DREAM KITCHEN</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit laudantium iste in harum veritatis ipsam minima saepe, ducimus assumenda tempora necessitatibus voluptates minus laboriosam id magnam labore inventore. Ullam, minus.</p>
+                    <div className="hero-button-container">
+                        <Link to="/portfolio" className="hero-work-button">View Our Work</Link>
+                        <Link to="/contact" className="hero-contact-button">Contact Us</Link>
+                    </div>
                 </div>
             </div>
+            <div className='carousel-container'>
+                <Carousel/>
+            </div>
+            <div className='hero-about-container'>
 
+            </div>
         </div>
-        <div className='carousel-container'>
-            <Carousel/>
-        </div>
-        <div className='hero-about-container'>
-
-        </div>
+       
         </>
     )
 }
