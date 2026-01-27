55e93a33008e15ce17a5b569157c36876c69567a
Simon Truong <78322463+simont-17@users.noreply.github.com>
2026-01-26 17:06:14 -0700

    built footer and did some styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import PortfolioPage from "./components/PortfolioPage";
 import ContactPage from "./components/ContactPage";
+import Footer from "./components/FooterBar";
 import "./App.css";
 import { BrowserRouter} from "react-router-dom";
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/portfolio" element={<PortfolioPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
