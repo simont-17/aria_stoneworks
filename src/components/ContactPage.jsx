5062e8edbac1325de40a2c5006d26e14af17401e
Zaarif-Sardar <zsard040@mtroyal.ca>
2026-01-31 19:32:14 -0700

    Contact form reform
    Altered the layout of the contact page. Looking better. Gotta iron out details and make functional

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -7,38 +7,52 @@ export default function ContactPage() {
         <>
             <div className="contact-hero-container">
                 <NavBar />
-                <div className="hero-text-container text-black ">
-                    <h1 className=" bg-white p-4 rounded-2xl">
-                        Contact us
-                    </h1>
+                <div className="contact-us-box flex justify-between p-8 ml-24 mr-24 mt-10 mb-10  size-fit rounded-2xl bg-gray-100  border-gray-300 rounded-3xl">
+                    <div className="  contact-us-text h-full flex justify-around flex-col w-2/5">
 
-                </div>
-            </div>
+                        <div className="flex flex-col gap-6">
+                            <h1 className="text-6xl font-medium ">Get in Touch with us!</h1>
 
-            <div className="contact-us-box flex m-24 rounded 2xl  border-2 border-solid border-gray-300 rounded-3xl">
-                <div className="  contact-us-text p-8  h-full w-full flex justify-center  flex-col ">
-                    <div className="flex flex-col gap-6">
-                        <h1 className="text-6xl font-medium ">Get in Touch with us!</h1>
+                            <p className=" text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque convallis id turpis eu sodales. Nullam molestie euismod dolor, sed varius lectus sodales dapibus. Aliquam at rhoncus leo. </p>
 
-                        <p className=" text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque convallis id turpis eu sodales. Nullam molestie euismod dolor, sed varius lectus sodales dapibus. Aliquam at rhoncus leo. </p>
-
-                        <div className="contact-options text-gray-600 text-lg">
-                            <p>Phone #: XXX-XXX-XXXX</p>
-                            <p>Email: BANDAR@gmail.com</p>
                         </div>
                         <div>
-                            <p className="text-lg font-medium underline ">Hours of Operations</p>
-                            <p className=" pl-4 text-md text-gray-600">Monday-Friday: 9am - 5pm</p>
+                            <div className="contact-options grid grid-cols-2 gap-6">
+                                <div>
+                                    <p className="text-xl">Location</p>
+                                    <p className="text-sm">121 Topaz gate, Chestermere AB</p>
+                                    <div>
+                                        <p className="text-md font-medium underline ">Hours of Operations</p>
+                                        <p className=" pl-4 text-md">Monday-Friday: 9am - 5pm</p>
+                                    </div>
+
+                                </div>
+
+                                <div>
+                                    <p className="text-xl">Phone</p>
+                                    <p className="text-sm">#: XXX-XXX-XXXX</p>
+                                </div>
+                                <div>
+                                    <p className="text-xl">Email</p>
+                                    <p className="text-sm">BANDAR@gmail.com</p>
+
+                                </div>
+
+                            </div>
+
                         </div>
 
 
-                    </div>
 
-                </div>
-                <div className="contact-form w-full h-full flex flex-col justify-center p-8">
-                    <Form />
-                </div>
-            </div>
+
+                    </div >
+                    <div className="contact-form  h-full flex flex-col  w-2/6">
+                        <Form />
+                    </div>
+                </div >
+            </div >
+
+
         </>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,30 +14,38 @@ export default function Form() {
     return (
         <>
 
-            <form onSubmit={handleSubmit(onSubmit)} className=" grid grid-cols-2 gap-6">
-                <div className=" flex flex-col justify-around">
-                    <label>First Name</label>
-                    <input className="border-2 border-gray-200 rounded-md" {...register("First name")}></input>
+            <form onSubmit={handleSubmit(onSubmit)} className=" col-span-2 flex flex-col gap-4 space-around p-4  bg-gray-50 border border-solid rounded-2xl h-full ">
+
+                <div className="grid grid-cols-2  gap-6">
+                    <h2 className="col-span-2 text-3xl ">Tell Us What you Need</h2>
+                    <p className="col-span-2">Our team is ready to assist you with every detail, big or small.</p>
+                    <div className=" flex flex-col justify-around col-span-1">
+                        <input className=" rounded-3xl pl-2 pt-6 pb-6 bg-white border  text-heading  rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs" {...register("First name")} placeholder="First Name"></input>
+                    </div>
+                    <div className=" flex flex-col justify-around col-span-1">
+                        <input className="rounded-3xl pl-2 pt-6 pb-6 " {...register("Last name")} placeholder="Last Name"></input>
+                    </div>
+                    <div className=" flex flex-col justify-around col-span-2">
+                        <input className=" rounded-3xl pl-2 pt-6 pb-6" {...register("Email")} placeholder="Email Address"></input>
+                    </div>
+                    <div className=" flex flex-col justify-around col-span-2">
+                        <input className="rounded-3xl pl-2 pt-6 pb-6" {...register("Phone Number")} placeholder="Phone Number"></input>
+                    </div>
+
+
                 </div>
-                <div className=" flex flex-col justify-around">
-                    <label>Last Name</label>
-                    <input className="border-2 border-gray-200 rounded-md" {...register("Last name")}></input>
+
+                <div className="flex flex-col gap-4 h-full">
+                    <h4 className="text-xl">
+                        Tell us what you need
+                    </h4>
+                    <div className=" flex flex-col col-span-2 h-full">
+
+                        <textarea className=" msgInput border-2 border-gray-200 rounded-2xl h-full p-2" {...register("Message")} placeholder="Message"></textarea>
+                    </div>
                 </div>
-                <div className=" flex flex-col justify-around col-span-2">
-                    <label>Email</label>
-                    <input className="border-2 border-gray-200 rounded-md" {...register("Email")}></input>
-                </div>
-                <div className=" flex flex-col justify-around col-span-2">
-                    <label>Phone Number</label>
-                    <input className="border-2 border-gray-200 rounded-md" {...register("Phone Number")}></input>
-                </div>
-                <div className=" flex flex-col justify-around col-span-2">
-                    <label>Message</label>
-                    <input className="border-2 border-gray-200 rounded-md" {...register("Message")}></input>
-                </div>
-                <div>
-                    <input type="submit" className=" bg-sky-300 rounded-md p-2 pr-4 pl-4"></input>
-                </div>
+                <input type="submit" className="  bg-sky-300 rounded-2xl p-4 w-full hover:bg-blue-700 font-bold text-gray-100 py-2 px-4 "></input>
+
             </form >
 
         </>
